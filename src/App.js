869e9fef74232f56ed63ba6cb378b98c1d27f28f
mostlyali07 <104094853+mostlyali07@users.noreply.github.com>
2023-02-20 23:34:31 +0500

    Update

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import React from "react";
+import { Link, useNavigate, Router } from "react-router-dom";
 
 const App = () => {
+  const navigate = useNavigate();
   return (
-    <>
+    <Router>
       <h1>Home</h1>
-    </>
+      <Link to={"/Login"}>Go To Login Page</Link>
+      <br />
+      <button onClick={() => navigate("/Login")}>Go To Signup Page</button>
+      <br />
+    </Router>
   );
 };
 export default App;
