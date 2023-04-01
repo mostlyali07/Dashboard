45c770d281c31c1c3fbbcddecdc6336104e41202
Syed Ali Rehan <104094853+mostlyali07@users.noreply.github.com>
2023-04-02 02:58:43 +0500

    update

--- src/App.js
+++ src/App.js
@@ -1,41 +1,23 @@
-// import React from "react";
-// import { Route, Routes, BrowserRouter } from "react-router-dom";
-// import Signup from "./Components/Signup";
-// import Login from "./Components/Login";
-// import Home from "./Components/Home";
-// import AllProducts from "./Components/AllProducts";
-
-// const App = () => {
-//   return (
-//     <>
-//       <BrowserRouter>
-//         <Routes>
-//           <Route path="/" element={<Home />} />
-//           <Route path="/signup" element={<Signup />} />
-//           <Route path="/login" element={<Login />} />
-//           <Route path="/all-products" element={<AllProducts />} />
-//         </Routes>
-//       </BrowserRouter>
-//     </>
-//   );
-// };
-// export default App;
-
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import LoginForm from "./Components/LoginForm";
+import React from "react";
+import { Route, Routes, BrowserRouter } from "react-router-dom";
+import Signup from "./Components/Signup";
+import Login from "./Components/Login";
+import Home from "./Components/Home";
 import AllProducts from "./Components/AllProducts";
-import ErrorPage from "./Components/ErrorPage";
 
-function App() {
+const App = () => {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<LoginForm />} />
-        <Route path="/all-products" element={<AllProducts />} />
-        <Route path="/error" element={<ErrorPage />} />
-      </Routes>
-    </Router>
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/all-products" element={<AllProducts />} />
+        </Routes>
+      </BrowserRouter>
+    </>
   );
-}
-
+};
 export default App;
+
--- src/Components/LoginForm.jsx
+++ src/Components/LoginForm.jsx
@@ -1,147 +1,75 @@
-// import React, { useEffect, useState } from "react";
-// import { signInWithEmailAndPassword } from "firebase/auth";
-// import "./../App.css";
-// import { Link } from "react-router-dom";
-// import { UserOutlined } from "@ant-design/icons";
-// import { Input, Button } from "antd";
-// import { auth, providers } from "../firebase"
-// import { signInWithPopup } from "firebase/auth";
-// import MainDashboard from "./MainDashboard";
-
-
-// const LoginForm = () => {
-//     const [email, setEmail] = useState("");
-//     const [password, setPassword] = useState("");
-//     const [value, setValue] = useState("");
-
-
-//     const signIn = (e) => {
-//         e.preventDefault();
-//         signInWithEmailAndPassword(auth, email, password)
-//             .then((userCredential) => {
-//                 console.log(userCredential);
-//             }).catch((error) => {
-//                 console.log(error);
-//             })
-//     }
-//     const handleClick = () => {
-//         signInWithPopup(auth, providers).then((data) => {
-//             setValue(data.user.email)
-//             localStorage.setItem("email", data.user.email)
-//         })
-//     }
-//     useEffect(() => {
-//         setValue(localStorage.getItem("email"))
-//     })
-//     return (
-//         <><form onSubmit={signIn}>
-//             <label htmlFor="Email"><b>Email</b></label><br />
-//             <Input
-//                 size="large"
-//                 placeholder="Email Address"
-//                 prefix={<UserOutlined />}
-//                 style={{
-//                     width: 300,
-//                 }}
-//                 value={email}
-//                 onChange={(e) => setEmail(e.target.value)}
-//             />
-//             <br />
-//             <br />
-//             <label htmlFor="Password"><b>Password</b></label><br />
-//             <Input.Password
-//                 size="large"
-//                 placeholder="input password"
-//                 style={{
-//                     width: 300,
-//                 }}
-//                 value={password}
-//                 onChange={(e) => setPassword(e.target.value)}
-//             />
-//             <br />
-//             <br />
-//             <Button htmlType="submit">Login</Button>
-//             <br />
-//             <br />
-//             <Link to="/signup">Sign Up</Link>
-//             <br />
-//             {value ? <MainDashboard /> :
-//                 <button onClick={handleClick}>Sign In With Google</button>
-//             }
-//         </form></>
-//     )
-// }
-
-// export default LoginForm;
-
-
 import React, { useEffect, useState } from "react";
-// import { useHistory } from "react-router-dom";
-import { useHistory } from 'react-router-dom';
-
 import { signInWithEmailAndPassword } from "firebase/auth";
+import "./../App.css";
+import { Link } from "react-router-dom";
 import { UserOutlined } from "@ant-design/icons";
 import { Input, Button } from "antd";
-import { auth } from "../firebase";
+import { auth, providers } from "../firebase"
+import { signInWithPopup } from "firebase/auth";
+import MainDashboard from "./MainDashboard";
+
 
 const LoginForm = () => {
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
-    const [error, setError] = useState(null);
-    const history = useHistory();
+    const [value, setValue] = useState("");
 
-  const signIn = (e) => {
-    e.preventDefault();
-    signInWithEmailAndPassword(auth, email, password)
-      .then((userCredential) => {
-        console.log(userCredential);
-        history.push("/all-products"); // redirect to all products page on successful login
-      })
-      .catch((error) => {
-        console.log(error);
-        setError(error.message); // set error message for display
-      });
-  };
 
-  return (
-    <>
-      {error && <div>{error}</div>} {/* display error message if there is one */}
-      <form onSubmit={signIn}>
-        <label htmlFor="Email">
-          <b>Email</b>
-        </label>
-        <br />
-        <Input
-          size="large"
-          placeholder="Email Address"
-          prefix={<UserOutlined />}
-          style={{
-            width: 300,
-          }}
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-        />
-        <br />
-        <br />
-        <label htmlFor="Password">
-          <b>Password</b>
-        </label>
-        <br />
-        <Input.Password
-          size="large"
-          placeholder="input password"
-          style={{
-            width: 300,
-          }}
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-        />
-        <br />
-        <br />
-        <Button htmlType="submit">Login</Button>
-      </form>
-    </>
-  );
-};
+    const signIn = (e) => {
+        e.preventDefault();
+        signInWithEmailAndPassword(auth, email, password)
+            .then((userCredential) => {
+                console.log(userCredential);
+            }).catch((error) => {
+                console.log(error);
+            })
+    }
+    const handleClick = () => {
+        signInWithPopup(auth, providers).then((data) => {
+            setValue(data.user.email)
+            localStorage.setItem("email", data.user.email)
+        })
+    }
+    useEffect(() => {
+        setValue(localStorage.getItem("email"))
+    })
+    return (
+        <><form onSubmit={signIn}>
+            <label htmlFor="Email"><b>Email</b></label><br />
+            <Input
+                size="large"
+                placeholder="Email Address"
+                prefix={<UserOutlined />}
+                style={{
+                    width: 300,
+                }}
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+            />
+            <br />
+            <br />
+            <label htmlFor="Password"><b>Password</b></label><br />
+            <Input.Password
+                size="large"
+                placeholder="input password"
+                style={{
+                    width: 300,
+                }}
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+            />
+            <br />
+            <br />
+            <Button htmlType="submit">Login</Button>
+            <br />
+            <br />
+            <Link to="/signup">Sign Up</Link>
+            <br />
+            {value ? <MainDashboard /> :
+                <button onClick={handleClick}>Sign In With Google</button>
+            }
+        </form></>
+    )
+}
 
 export default LoginForm;
